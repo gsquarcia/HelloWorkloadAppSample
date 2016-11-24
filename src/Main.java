ae31d918753c801971d0b714ac6bae8073083177
it042498 <it042498@IBM362-R9VKTV6>
2016-11-24 12:30:26 +0100

    variables definition added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 
 public class Main {
-
+	int a;
+	int b;
+	String c;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ciaos");
 	}
+	
+	
 
 }
