1a5e61c0f76ccdc05951fd23a5873a7e09d95e88
it042498 <it042498@IBM362-R9VKTV6>
2016-11-24 12:48:14 +0100

    added printHello

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,12 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ciaos");
+		printHello("ciao");
 	}
 	
+	public static void printHello(String msg){
+		System.out.println(msg);
+	}
 	
 
 }
